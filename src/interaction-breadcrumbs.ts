db3ee7ee256eff5c7adb78d82fe41e473189642a
Brian Birtles <birtles@gmail.com>
2024-02-22 12:31:28 +0900

    fix: truncate node text before tidying up whitespace
    Fixes #135.

--- src/interaction-breadcrumbs.ts
+++ src/interaction-breadcrumbs.ts
@@ -53,8 +53,9 @@ function getNodeText(elem: HTMLElement): string {
   ) {
     text = (elem as HTMLInputElement).value;
   }
-  text = text.replace(/^\s+|\s+$/g, ''); // trim whitespace
-  return truncate(text, 140);
+  // Truncate text first to avoid running our whitespace regex on a large string
+  text = truncate(text, 140);
+  return text.replace(/^\s+|\s+$/g, ''); // trim whitespace
 }
 
 // Create a label from tagname, id and css class of the element
@@ -81,7 +82,7 @@ function getNodeSelector(elem: Element): string {
     if (self.document.querySelectorAll(parts.join('')).length === 1) {
       return parts.join('');
     }
-  } catch (e) {
+  } catch {
     // Sometimes the query selector can be invalid just return it as-is.
     return parts.join('');
   }
