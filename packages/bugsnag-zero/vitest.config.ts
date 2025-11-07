8629b39700a52ab8f7c11c91ea234b198a0b7bdf
Brian Birtles <birtles@gmail.com>
2025-11-07 13:26:53 +0900

    chore: drop no-longer-needed vitest-github-actions-reporter

--- packages/bugsnag-zero/vitest.config.ts
+++ packages/bugsnag-zero/vitest.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vitest/config';
-import GithubActionsReporter from 'vitest-github-actions-reporter';
 
 export default defineConfig({
   test: {
     include: ['./src/**/*.test.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: process.env.GITHUB_ACTIONS
-      ? ['default', new GithubActionsReporter()]
-      : 'default',
   },
 });
