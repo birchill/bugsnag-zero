00dbc334482a0abbf78cf0ce5c83bdd594b026c2
Brian Birtles <birtles@gmail.com>
2026-01-23 16:11:48 +0900

    chore: restore existing filenames

--- packages/bugsnag-zero-lambda-context/tsdown.config.ts
+++ packages/bugsnag-zero-lambda-context/tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
+  fixedExtension: false,
   format: ['cjs', 'esm'],
   sourcemap: true,
 });
--- packages/bugsnag-zero/tsdown.config.ts
+++ packages/bugsnag-zero/tsdown.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   dts: true,
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  fixedExtension: false,
   sourcemap: true,
   outputOptions: {
     // Indicate that we are intentionally mixing named and default exports in
